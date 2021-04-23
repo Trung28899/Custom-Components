a1628de46249cdd6c4f1c358ae811841de9c2968
Trung28899 <trung28899@gmail.com>
2021-04-23 14:03:15 -0400

    embeded youtube component done

--- src/components/EmbeddedModal/EmbeddedModal.js
+++ src/components/EmbeddedModal/EmbeddedModal.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Embedded.module.css";
 import ReactPlayer from "react-player/lazy";
+import SimpleTextBox from "../SimpleTextBox/SimpleTextBox";
 
 const EmbeddedModal = () => {
-  const url = "https://www.youtube.com/watch?v=WYRbdJhzj2M";
+  const [url, setUrl] = useState("");
+  const onChangeUrl = (value) => {
+    setUrl(value);
+  };
+
   return (
     <div className={classes.Modal}>
-      <div className={classes.output}>
-        <ReactPlayer url={url} controls={true} width="100%" height="100%" />
+      <div className={classes.closeButton}>
+        <i className="fas fa-times"></i>
       </div>
-      <div className={classes.input}></div>
+      <h3 className={classes.title}>
+        Nhúng <span>Youtube</span> Video
+      </h3>
+      <div className={classes.output}>
+        <ReactPlayer
+          url={url || " "}
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div className={classes.input}>
+        <SimpleTextBox
+          textHolder="Nhập đường link Youtube..."
+          textEntered={onChangeUrl}
+          modalShow
+        />
+      </div>
+      <div className={classes.button}>Add Link</div>
     </div>
   );
 };
